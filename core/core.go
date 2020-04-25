a9563c5da8177bdfd095771da729e063da95eb5a
Tongchai Laoverakul <bird@pixalione.com>
2020-04-25 10:40:45 +0700

    Add core

---
+++ core/core.go
@@ -0,0 +1,30 @@
+// Package core orchestrates every parts.
+// It holds the domain logic.
+package core
+
+// STATE0 is the initial state.
+const STATE0 = State{}
+
+var (
+	sim   = Simulation{}
+	guide = Guide{sim}
+	ai    = Agent{StateDimension: guide.EndDimension, ActionDimension: sim.ActionDimension}
+	state = STATE0
+)
+
+// Restart sets state back to the inital state.
+func Restart() {
+	state = STATE0
+}
+
+// Step moves the simulation forward in time, and returns this next state.
+func Step() State {
+	action := ai.Act(state)
+	state = sim.Simulate(state, action)
+	return state
+}
+
+// Train forces ai to get better at its job.
+func Train() {
+	ai.Train()
+}
